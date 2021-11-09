ac91e97cc8a1091ba0e5f326c14af05ba7252512
chooper1 <54963573+chooper1@users.noreply.github.com>
2021-11-09 18:59:28 -0500

    Waitlist working

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 // const navBtnContainer = document.querySelector(".nav-btn-container");
 // const searchBtn = document.querySelector(".search-btn");
 // const closeBtn = document.querySelector(".close-btn");
+const signUpBtn = document.querySelector(".image4");
+// const codecamp = document.querySelector(".codecamp");
 
 // searchBtn.addEventListener("click", () => {
 //   searchBox.classList.add("active");
@@ -13,16 +15,24 @@
 //   navBtnContainer.classList.remove("active");
 // });
 
-const waitBtn = document.querySelector(".Waitlist");
-waitBtn.addEventListener("click", () => {
-  window.open('http://google.com', "_blank");
-});
+// const waitBtn = document.querySelector(".Waitlist");
+// waitBtn.addEventListener("click", () => {
+//   window.open('http://google.com', "_blank");
+// });
 
+// signUpBtn.onclick = window.open('https://www.espn.com/', "_blank");
 
-document.getElementById("Waitlist").onclick = window.open('http://google.com', "_blank");
+// document.getElementById("Waitlist").onclick = window.open('http://google.com', "_blank");
 
-function myFunction() {
-    window.open('http://www.website.com/page', "_blank");
+// function myFunction() {
+//     window.open('http://www.website.com/page', "_blank");
+// }
+
+function signuponclick() {
+  window.open('https://docs.google.com/forms/d/e/1FAIpQLSejB7Zo9Av11H3maMwCpNWo75k168WIX9O4JG6Mn-SNOhZSWA/viewform?usp=sf_link', "_blank");
 }
 
+// function changeColor() {
+//   codecamp.style.color = "blue";
+// }
 
