a0a62ca9bb0a0545e5d71cbcffb3e270ffa17ab4
chooper1 <54963573+chooper1@users.noreply.github.com>
2021-11-09 14:02:26 -0500

    Added site

---
+++ main.js
@@ -0,0 +1,28 @@
+// const searchBox = document.querySelector(".search-box");
+// const navBtnContainer = document.querySelector(".nav-btn-container");
+// const searchBtn = document.querySelector(".search-btn");
+// const closeBtn = document.querySelector(".close-btn");
+
+// searchBtn.addEventListener("click", () => {
+//   searchBox.classList.add("active");
+//   navBtnContainer.classList.add("active");
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   searchBox.classList.remove("active");
+//   navBtnContainer.classList.remove("active");
+// });
+
+const waitBtn = document.querySelector(".Waitlist");
+waitBtn.addEventListener("click", () => {
+  window.open('http://google.com', "_blank");
+});
+
+
+document.getElementById("Waitlist").onclick = window.open('http://google.com', "_blank");
+
+function myFunction() {
+    window.open('http://www.website.com/page', "_blank");
+}
+
+
