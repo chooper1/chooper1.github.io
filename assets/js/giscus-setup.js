e5a5e56149241443240a7aedd48e47cc763eef56
Coleman Hooper <chooper@wifi-10-41-20-92.wifi.berkeley.edu>
2025-10-07 14:06:56 -0700

    Publish built site

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "chooper1/chooper1.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
